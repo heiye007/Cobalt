9cc072d3d120aef9a188ea0ae22e7dab2a0561b8
cakehonolulu <cakehonolulu@users.noreply.github.com>
2017-08-22 16:34:27 +0200

    Kernel init refactoring

--- core/kernel.c
+++ core/kernel.c
@@ -4,13 +4,13 @@
 #define CHECK_FLAG(flags,bit)   ((flags) & (1 << (bit)))
 
 void init(unsigned long magic, unsigned long addr) {
-	multiboot_info_t *mbi;
+	//multiboot_info_t *mbi;
 	init_vga();
 	if (magic != MULTIBOOT_BOOTLOADER_MAGIC) {
 		printf("Invalid magic number: 0x%x\n", (unsigned) magic);
 		return;
 	}
-	mbi = (multiboot_info_t *) addr;
+	/*mbi = (multiboot_info_t *) addr;
 	uint16_t mem_lower = mbi->mem_lower;
 	uint16_t mem_higher = mbi->mem_upper;
 	uint16_t totalmem = mem_lower+mem_higher;
@@ -18,19 +18,22 @@ void init(unsigned long magic, unsigned long addr) {
 	if (CHECK_FLAG(mbi->flags, 0))
 	printf("mem_lower = %uKB, mem_upper = %uKB\n",
 		(unsigned) mbi->mem_lower, (unsigned) mbi->mem_upper);
-	printf("totalmem = %uKB , in MB: %uMB\n", totalmem+1024, (totalmem+1024)/1024);
+	printf("totalmem = %uKB , in MB: %uMB\n", totalmem+1024, (totalmem+1024)/1024);*/
 	init_gdt();
-	printf("GDT Set\n");
+	printkok("Initialized GDT");
 	init_idt();
-	printf("IDT Set\n");
+	printkok("Initialized IDT");
 	init_isr();
-	printf("ISR Set\n");
+	printkok("Initialized ISR's");
 	init_irq();
-	printf("IRQ Set\n");
+	printkok("Initialized IRQ's");
 	__asm__ __volatile__ ("sti");
-	keyboard_install();
+	//init_timer();
+	//detectCPU();
+	printkok("Initialized Keyboard");
+	init_keyboard();
+	shell();
 	while(1);
-	//timer_install();
 	//int i = 10 / 0;
 	//printk(i);
 	//init_timer(50);
