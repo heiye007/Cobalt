0949f0d9d98e746373d5542f0a7a3766c30d6d38
cakehonolulu <cakehonolulu@users.noreply.github.com>
2017-08-22 16:31:01 +0200

    IRQ header refactor

--- include/i386/irq.h
+++ include/i386/irq.h
@@ -21,12 +21,10 @@ void *irq_routines[16] =
     0, 0, 0, 0, 0, 0, 0, 0
 };
 
-void irq_install_handler(int irq, void (*handler)(struct regs *r))
-{
+void irq_install_handler(int irq, void (*handler)(struct regs *r)) {
     irq_routines[irq] = handler;
 }
 
-void irq_uninstall_handler(int irq)
-{
+void irq_uninstall_handler(int irq) {
     irq_routines[irq] = 0;
 }
