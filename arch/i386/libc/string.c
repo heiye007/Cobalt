b7968774f2ddc27184367b33b29e4854c45de589
cakehonolulu <cakehonolulu@users.noreply.github.com>
2017-08-22 16:29:00 +0200

    Add strcmp

--- arch/i386/libc/string.c
+++ arch/i386/libc/string.c
@@ -34,3 +34,18 @@ void *memset(void* s, int c, size_t n) {
   }
   return s;
 }
+
+/* Simple function to compare two C strings */
+// UNIX Compatible
+int strcmp(const char *s1, const char *s2) {
+  int i = 0;
+  while(s1[i] == s2[i] && s1[i] != '\0' && s2[0] != '\0') {
+    i++;
+  }
+
+  if (s1[i] == '\0' && s2[i] == '\0') {
+    return 0;
+  }
+
+  return 1;
+}
