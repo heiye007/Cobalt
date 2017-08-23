b58fda0a0e79b546b5182c962a568bdaa58bec43
cakehonolulu <cakehonolulu@users.noreply.github.com>
2017-08-23 15:01:56 +0200

    VGA Header

---
+++ include/vga.h
@@ -0,0 +1,18 @@
+enum vga_color {
+    BLACK = 0,
+    BLUE = 1,
+    GREEN = 2,
+    CYAN = 3,
+    RED = 4,
+    MAGENTA = 5,
+    BROWN = 6,
+    LGRAY = 7,
+    DGRAY = 8,
+    LBLUE = 9,
+    LGREEN = 10,
+    LCYAN = 11,
+    LRED = 12,
+    LMAGENTA = 13,
+    YELLOW = 14,
+    WHITE = 15
+};
