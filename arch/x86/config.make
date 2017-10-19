7dfecb264f63a222b52fc9df2681e55cbaf30c44
cakehonolulu <cakehonolulu@protonmail.com>
2017-10-19 22:25:06 +0200

    kernel: Remove skeleton files

--- arch/x86/config.make
+++
@@ -1,3 +0,0 @@
-LINKER= arch/$(ARCH)/linker.ld
-CFLAG= -I./include -ffreestanding -Wall -Wextra
-LDFLAG= -nostdlib -T $(LINKER) -o $(KERNEL) $(OBJS) -lgcc
--- arch/x86/libc/cpuid.c
+++
@@ -1,7 +0,0 @@
-#include <stdint.h>
-
-void cpuid(uint16_t reg, uint16_t *eax, uint16_t *ebx, uint16_t *ecx, uint16_t *edx) {
-    __asm__ __volatile__("cpuid"
-        : "=a" (*eax), "=b" (*ebx), "=c" (*ecx), "=d" (*edx)
-        : "0" (reg));
-}
