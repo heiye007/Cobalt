a0a33552ce307d40a0a325a9040ce42e7b381a8f
cakehonolulu <cakehonolulu@protonmail.com>
2017-10-21 14:07:17 +0200

    kernel: Separate timer into PIC and PIT

---
+++ arch/i386/sys/pic.c
@@ -0,0 +1,29 @@
+#include <stdint.h>
+#include <i386/pic.h>
+
+void pic_init()
+{
+#ifdef DBG_PIT
+    printk("Starting PIT initialization...\n");
+#endif
+    outb(PIC_MASTER_CMD, 0x10 + 0x01);
+    outb(PIC_SLAVE_CMD,  0x10 + 0x01);
+    outb(PIC_MASTER_DATA, 0x20);
+    outb(PIC_SLAVE_DATA, 0x28);
+    outb(PIC_MASTER_DATA, 4);
+    outb(PIC_SLAVE_DATA, 2);
+    outb(PIC_MASTER_DATA, 0x01);
+    outb(PIC_SLAVE_DATA, 0x01);
+    outb(PIC_MASTER_DATA, 0);
+    outb(PIC_SLAVE_DATA, 0);
+#ifdef DBG_PIT
+    printk("Started PIT successfully...\n");
+#endif
+}
+
+void pic_send_eoi(uint8_t irq)
+{
+    if(irq >= 8)
+        outb(PIC_SLAVE_CMD, PIC_CMD_EOI);
+    outb(PIC_MASTER_CMD, PIC_CMD_EOI);
+}
