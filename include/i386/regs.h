4b3f7314690e0a8477e66e6e351545586e72c3bd
cakehonolulu <cakehonolulu@protonmail.com>
2018-01-19 07:39:22 +0100

    include/i386: Add registers_t

--- include/i386/regs.h
+++ include/i386/regs.h
@@ -6,3 +6,11 @@ struct regs {
     uint16_t int_no, err_code;
     uint16_t eip, cs, eflags, useresp, ss;
 };
+
+typedef struct registers
+{
+    uint32_t ds;
+    uint32_t edi, esi, ebp, esp, ebx, edx, ecx, eax;
+    uint32_t int_no, err_code;
+    uint32_t eip, cs, eflags, useresp, ss;
+} registers_t;
