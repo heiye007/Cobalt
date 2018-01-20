f56ce916c7c3310abac5f48fa367ca0fe9dad428
cakehonolulu <cakehonolulu@protonmail.com>
2018-01-20 22:26:54 +0100

    i386/sys/gdt: Regularize programming style

--- arch/i386/sys/gdt.c
+++ arch/i386/sys/gdt.c
@@ -1,6 +1,7 @@
 #include <i386/gdt.h>
 
-void gdt_set_gate(int num, size_t base, size_t limit, uint8_t access, uint8_t gran) {
+void gdt_set_gate(int num, size_t base, size_t limit, uint8_t access, uint8_t gran)
+{
     gdt[num].base_low = (base & 0xFFFF);
     gdt[num].base_middle = (base >> 16) & 0xFF;
     gdt[num].base_high = (base >> 24) & 0xFF;
@@ -10,7 +11,8 @@ void gdt_set_gate(int num, size_t base, size_t limit, uint8_t access, uint8_t gr
     gdt[num].access = access;
 }
 
-void init_gdt(void) {
+void init_gdt(void)
+{
     gp.limit = (sizeof(struct gdt_entry) * 3) - 1;
     gp.base = &gdt;
     gdt_set_gate(0, 0, 0, 0, 0);
