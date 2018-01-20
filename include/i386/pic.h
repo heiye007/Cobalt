ff2351b9a8b58ab3b5b1dceadb6911288b4e5f5d
cakehonolulu <cakehonolulu@protonmail.com>
2018-01-20 22:59:01 +0100

    include/i386/pic: Unify programming style

--- include/i386/pic.h
+++ include/i386/pic.h
@@ -1,10 +1,10 @@
+#include <stdint.h>
+
 #define PIC_MASTER_CMD 0x20
 #define PIC_MASTER_DATA 0x21
 #define PIC_SLAVE_CMD 0xA0
 #define PIC_SLAVE_DATA 0xA1
 #define PIC_CMD_EOI 0x20
-
-#include <stdint.h>
 
 #define PIC1_REG_COMMAND 0x20
 #define PIC1_REG_STATUS 0x20
