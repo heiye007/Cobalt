c9e1beb634a3bb99154ec0bee5b3f93ec4aca23b
cakehonolulu <cakehonolulu@protonmail.com>
2018-01-20 22:27:18 +0100

    i386/sys/idt: Regularize programming style

--- arch/i386/sys/idt.c
+++ arch/i386/sys/idt.c
@@ -1,6 +1,7 @@
 #include <i386/idt.h>
 
-void idt_set_gate(uint8_t num, size_t base, unsigned short sel, uint8_t flags) {
+void idt_set_gate(uint8_t num, size_t base, unsigned short sel, uint8_t flags)
+{
     idt[num].base_lo = (base & 0xFFFF);
     idt[num].base_hi = (base >> 16) & 0xFFFF;
     idt[num].sel = sel;
@@ -8,7 +9,8 @@ void idt_set_gate(uint8_t num, size_t base, unsigned short sel, uint8_t flags) {
     idt[num].flags = flags;
 }
 
-void init_idt(void) {
+void init_idt(void)
+{
     idtp.limit = (sizeof (struct idt_entry) * 256) - 1;
     idtp.base = &idt;
     memset(&idt, 0, sizeof(struct idt_entry) * 256);
