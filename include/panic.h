b30c4a01aa428973d907aaed59eb712c4d66fb11
cakehonolulu <cakehonolulu@protonmail.com>
2018-01-20 22:57:21 +0100

    include/panic: Unify programming style

--- include/panic.h
+++ include/panic.h
@@ -1,4 +1,5 @@
 #include <stdint.h>
 
-extern void panic(char* err, const char *file, uint32_t line);
 #define PANIC(err) panic(err, __FILE__, __LINE__);
+
+extern void panic(char* err, const char *file, uint32_t line);
