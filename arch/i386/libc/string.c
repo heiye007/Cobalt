d2d6b461eae8bcf5443a580c3bfe5c298f297a5d
cakehonolulu <cakehonolulu@protonmail.com>
2018-01-21 01:06:10 +0100

    i386/libc/string: Introduce strncpy & strcpy

--- arch/i386/libc/string.c
+++ arch/i386/libc/string.c
@@ -67,3 +67,31 @@ int strcmp(const char *s1, const char *s2)
 
   return 1;
 }
+
+/* Function to copy a string */
+// UNIX Compatible
+char *strcpy(char *dest, const char *src)
+{
+  char *p = dest;
+
+  while (*src)
+    *p++ = *src++;
+  *p = '\0';
+
+  return dest;
+}
+
+/* Function to copy 'n' bytes from a string */
+// UNIX Compatible
+char *strncpy(char *dest, const char *src, size_t n)
+{
+  size_t i;
+
+  for (i = 0; i < n && src[i] != '\0'; i++)
+    dest[i] = src[i];
+  
+  for ( ; i < n; i++)
+    dest[i] = '\0';
+
+   return dest;
+}
