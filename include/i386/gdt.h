5e332f141a5677d35cb8b05e3a1f8d77504466ad
cakehonolulu <cakehonolulu@protonmail.com>
2018-01-28 21:51:46 +0100

    include/i386/gdt: Add comments to the code

--- include/i386/gdt.h
+++ include/i386/gdt.h
@@ -1,5 +1,10 @@
 #include <stdint.h>
 
+/* cake: We use __attribute__((packed))
+   in order to prevent the compiler
+   from optimizing the code in any way */
+
+/* GDT scheme */
 struct gdt_entry
 {
     unsigned short limit_low;
@@ -10,13 +15,20 @@ struct gdt_entry
     uint8_t base_high;
 } __attribute__((packed));
 
+/* GDT's pointer scheme */
 struct gdt_ptr
 {
     unsigned short limit;
     uint16_t base;
 } __attribute__((packed));
 
+/* GDT structure */
 struct gdt_entry gdt[3];
+
+/* GDT pointer */
 struct gdt_ptr gp;
 
+/* cake: Function declared on another file
+   to reload the segment registers and swap
+   out CPU's GDT table with ours */
 extern void gdt_flush();		
