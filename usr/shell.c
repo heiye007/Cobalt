6bd9f31075cf80f2ea466024a6ba418c31da13d5
cakehonolulu <cakehonolulu@protonmail.com>
2018-01-28 19:45:43 +0100

    usr/shell: Hack/Semifix backspacing row deletion, needs proper buffer-based patch, but works for now

--- usr/shell.c
+++ usr/shell.c
@@ -1,47 +1,63 @@
 #include <stdint.h>
 
+int lastlinedetect;
+
 uint32_t shell()
 {
   char c, line[80], cmd[80], arg[80];
-  int i;
   printf("Running Shell...\n");
 
   while(1)
   {
     printf("$ ");
-    i = 0;
+    lastlinedetect = 0;
     memset(line, '\0', 80);
     memset(cmd, '\0', 80);
     memset(arg, '\0', 80);
 
     while((c = getch()) != '\n')
     {
+      // cake: Well, backspacing 'works' using
+      // nasty hacks, we can't rely on integer
+      // comparison for checking if we 'arrived'
+      // to the shell sign. We should use a buffer
+      // so it stays as a known-bug without a proper fix.
       if (c == '\b')
       {
-        if (i == 0) continue;
-        i--;
-        update_cursor(get_row()-1, get_col());
-        putch(' ');
+        if (lastlinedetect == 0) {
+          continue;
+        } else {
+          if (get_row() == 0) {
+            lastlinedetect--;
+            update_cursor(79, get_col()-1);
+            putch(' ');
+            update_cursor(80, get_col()-1);
+          } else {
+            lastlinedetect--;
+            update_cursor(get_row()-1, get_col());
+            putch(' ');
+        }
+        }
       }
       else
       {
-        line[i] = c;
-        i++;
+        line[lastlinedetect] = c;
+        lastlinedetect++;
       }
       putch(c);
     }
 
     putch(c);
-    line[i] = '\0';
+    line[lastlinedetect] = '\0';
 
     int pos = 0;
 
     while(line[pos] != ' ' && line[pos] != '\0') pos++;
 
     memcpy(cmd, line, pos);
-    memcpy(arg, line + pos + 1, i - pos);
+    memcpy(arg, line + pos + 1, lastlinedetect - pos);
     cmd[pos] = '\0';
-    arg[i - pos] = '\0';
+    arg[lastlinedetect - pos] = '\0';
 
     if (!strcmp(cmd, "hello"))
     {
