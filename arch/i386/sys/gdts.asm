00e25cd1c5cbe3a63711d36a8543e64ad4da619d
cakehonolulu <cakehonolulu@protonmail.com>
2018-01-28 21:44:39 +0100

    i386/sys/gdt: Add comments to the code

--- arch/i386/sys/gdts.asm
+++ arch/i386/sys/gdts.asm
@@ -1,13 +1,13 @@
-global gdt_flush
-extern gp
+global gdt_flush	; Allow 'gdt_flush' to be called from C
+extern gp			; gp is declared on C code
 gdt_flush:
-    lgdt [gp]
-    mov ax, 0x10
+    lgdt [gp]		; Load the GDT with our gp pointer
+    mov ax, 0x10	; 0x10 is our data segment offset
     mov ds, ax
     mov es, ax
     mov fs, ax
     mov gs, ax
     mov ss, ax
-    jmp 0x08:flush2
+    jmp 0x08:flush2	; 0x08 is our code segment offset
 flush2:
-    ret
+    ret				; Return to C code
