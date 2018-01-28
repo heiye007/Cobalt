916d345880cc0b6b8458b94d919b46fa7c91db19
cakehonolulu <cakehonolulu@protonmail.com>
2018-01-28 22:06:12 +0100

    include/i386/idt: Add comments to the code

--- include/i386/idt.h
+++ include/i386/idt.h
@@ -1,5 +1,10 @@
 #include <stdint.h>
 
+/* cake: We use __attribute__((packed))
+   in order to prevent the compiler
+   from optimizing the code in any way */
+
+/* IDT scheme */
 struct idt_entry
 {
     unsigned short base_lo;
@@ -9,13 +14,19 @@ struct idt_entry
     unsigned short base_hi;
 } __attribute__((packed));
 
+/* IDT's pointer scheme */
 struct idt_ptr
 {
     unsigned short limit;
     uint16_t base;
 } __attribute__((packed));
 
+/* IDT structure */
 struct idt_entry idt[256];
+
+/* IDT pointer */
 struct idt_ptr idtp;
 
+/* cake: Function declared on another file
+   to load our IDT table */
 extern void idt_load();
