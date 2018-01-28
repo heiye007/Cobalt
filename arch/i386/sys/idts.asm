53786db518d5d81f9d838657fe260afaf7253af3
cakehonolulu <cakehonolulu@protonmail.com>
2018-01-28 22:05:54 +0100

    i386/sys/idt: Add comments to the code

--- arch/i386/sys/idts.asm
+++ arch/i386/sys/idts.asm
@@ -1,5 +1,5 @@
-global idt_load
-extern idtp
+global idt_load	; Allow 'idt_load' to be called from C
+extern idtp		; idtp is declared on C code
 idt_load:
-    lidt [idtp]
-    ret
+    lidt [idtp]	; Load the IDT with our idtp pointer
+    ret			; Return to C code
