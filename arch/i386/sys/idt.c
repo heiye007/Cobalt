6d5bcf4b3ce04368d7d33ee97b5c0f08aa8ef37f
cakehonolulu <cakehonolulu@protonmail.com>
2018-01-28 22:01:06 +0100

    i386/sys/idt: Add comments to the code

--- arch/i386/sys/idt.c
+++ arch/i386/sys/idt.c
@@ -9,14 +9,31 @@ void idt_set_gate(uint8_t num, size_t base, unsigned short sel, uint8_t flags)
     idt[num].flags = flags;
 }
 
+/* Called on architecture init.
+   Setups the IDT, initializes the ISR's
+   and IRQ's within and finally enables
+   interrupts */
 void init_idt(void)
 {
+    /* Setup IDT limit */
     idtp.limit = (sizeof (struct idt_entry) * 256) - 1;
+
+    /* Setup IDT pointer */
     idtp.base = &idt;
+
+    /* Clear the IDT by zeroing out it */
     memset(&idt, 0, sizeof(struct idt_entry) * 256);
+
+    /* Init ISR's */
     init_isr();
+
+    /* Init IRQ's */
     init_irq();
+
+    /* Replace old IDT with the new one by 
+       flushing all the changes */
     idt_load();
 
+    /* Enable Interrupts */
     __asm__ __volatile__ ("sti");
 }
