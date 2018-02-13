a5f94c63afc7021e4a929990741984c1b7bcd173
cakehonolulu <cakehonolulu@protonmail.com>
2018-02-13 07:56:13 +0100

    include/i386: Track new header files

---
+++ include/i386/cpuid.h
@@ -0,0 +1,3 @@
+#include <stdint.h>
+
+void cpuid(uint16_t reg, uint16_t *eax, uint16_t *ebx, uint16_t *ecx, uint16_t *edx);
---
+++ include/string.h
@@ -0,0 +1,8 @@
+#include <stdint.h>
+
+size_t strlen(const char *s);
+void *memcpy(void *dest, const void *src, size_t n);
+void *memset(void* s, int c, size_t n);
+int strcmp(const char *s1, const char *s2);
+char *strcpy(char *dest, const char *src);
+char *strncpy(char *dest, const char *src, size_t n);
