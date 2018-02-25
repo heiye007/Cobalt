965d85e542adfc3be4c6f13a8cc0e6ce8831d150
cakehonolulu <cakehonolulu@protonmail.com>
2018-02-25 14:19:07 +0100

    i386/sys/8253: Chipset info

--- arch/i386/sys/8253.c
+++ arch/i386/sys/8253.c
@@ -149,5 +149,5 @@ void pit_init(void)
 {
     irq_install_handler(0, timer_handler);
     timer_phase(100); // Set to 100Hz
-    printkok("Initialized PIT");
+    printkok("Initialized PIT (8253)");
 }
