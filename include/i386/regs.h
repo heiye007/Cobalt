a2d8b75f65bdcb492923f7d50826095d6c27df72
cakehonolulu <cakehonolulu@users.noreply.github.com>
2018-08-27 13:07:11 +0200

    i386/regs: Minimal tweaking

--- include/i386/regs.h
+++ include/i386/regs.h
@@ -1,9 +1,21 @@
+#ifndef _REGS_H_
+#define _REGS_H_
+
 #include <stdint.h>
 
 struct regs
 {
-    uint16_t gs, fs, es, ds;
-    uint16_t edi, esi, ebp, esp, ebx, edx, ecx, eax;
-    uint16_t int_no, err_code;
-    uint16_t eip, cs, eflags, useresp, ss;
+    uint32_t gs, fs, es, ds;
+    uint32_t edi, esi, ebp, csc, ebx, edx, ecx, eax;
+    uint32_t int_no, err_code;
+    uint32_t eip, cs, eflags, esp, ss;
 };
+
+typedef struct {
+   uint32_t ds;
+   uint32_t edi, esi, ebp, csc, ebx, edx, ecx, eax;
+   uint32_t int_no, err_code;
+   uint32_t eip, cs, eflags, esp, ss;
+} registers_t;
+
+#endif
