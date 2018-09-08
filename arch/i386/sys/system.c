a646213b9b40e664e8eba6b570021a28273742e2
cakehonolulu <cakehonolulu@users.noreply.github.com>
2018-09-08 11:44:27 +0200

    i386/system: Add few functions

--- arch/i386/sys/system.c
+++ arch/i386/sys/system.c
@@ -11,3 +11,15 @@ void outb (unsigned short _port, uint8_t _data)
 {
     __asm__ __volatile__ ("outb %1, %0" : : "dN" (_port), "a" (_data));
 }
+
+void outw(uint16_t _data, uint16_t _port)
+{
+	__asm__ __volatile__ ("outw %w0, %w1" : : "a" (_data), "Nd" (_port));
+}
+
+uint16_t inw(uint16_t _port)
+{
+	uint16_t _data;
+	__asm__ __volatile__ ("inw %w1, %w0" : "=a"(_data) : "Nd" (_port));
+	return _data;
+}
